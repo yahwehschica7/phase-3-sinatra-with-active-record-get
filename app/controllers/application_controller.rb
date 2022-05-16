220c84778b9be687d9ca5dbbba3f5e3e95818c98
Jessica Nelson <yahwehschica@yahoo.com>
2022-05-16 15:30:05 -0400

    commit tree aa8f5d277c3248f317738a863b0974001cb3b7f4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10) 
+    games.to_json
+  end
+
+  get '/games/:id' do 
+    game = Game.find(params[:id])
+    game.to_json(include: {reviews: {include: :user} })
   end
 
 end
